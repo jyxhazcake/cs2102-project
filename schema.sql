2bcc28b48e438753e4c0166c32714b896b322f3e
Jon-the-MELON <limyongkiat@yahoo.com.sg>
2021-10-29 02:07:35 +0800

    updated triggers for constraints 12, 16, 18, 19, 21, 23, 34

--- schema.sql
+++ schema.sql
@@ -108,3 +108,134 @@ CREATE TABLE Approves (
    FOREIGN KEY (date, time, room, floor) REFERENCES Sessions (date, time, room, floor)
    ON DELETE CASCADE
 );
+
+--FIXES 12
+CREATE OR REPLACE FUNCTION block_junior_booking() RETURNS TRIGGER AS $$
+DECLARE
+    count NUMERIC;
+BEGIN
+    SELECT COUNT(*) into count;
+    FROM Junior
+    WHERE NEW.eid = Junior.eid;
+
+    IF count > 0 THEN
+        RETURN NULL;
+    ELSE
+        RETURN NEW;
+    END IF;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER junior_employee_cannot_book_room
+BEFORE INSERT OR UPDATE ON Books
+FOR EACH ROW
+EXECUTE FUNCTION block_junior_booking();
+
+--FIXES 16 AND 19
+CREATE OR REPLACE FUNCTION block_fever_meeting() RETURNS TRIGGER AS $$
+DECLARE
+    count NUMERIC;
+BEGIN
+    SELECT COUNT(*) into count;
+    FROM Health_Declaration
+    WHERE NEW.eid = Health_Declaration.eid AND Health_Declaration.fever = true;
+
+    IF count > 0 THEN
+        RETURN NULL;
+    ELSE
+        RETURN NEW;
+    END IF;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER fever_cannot_book_room
+BEFORE INSERT OR UPDATE ON Books
+FOR EACH ROW EXECUTE FUNCTION block_fever_meeting();
+
+CREATE TRIGGER fever_cannot_join_room
+BEFORE INSERT OR UPDATE ON Joins
+FOR EACH ROW EXECUTE FUNCTION block_fever_meeting();
+
+--FIXES 18
+CREATE OR REPLACE FUNCTION booker_joins_meeting() RETURNS TRIGGER AS $$
+BEGIN
+    INSERT INTO Joins VALUES (NEW.eid, NEW.date, NEW.time, NEW.room, NEW.floor);
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER the_booker_must_join_meeting
+AFTER INSERT OR UPDATE ON Books
+FOR EACH ROW EXECUTE FUNCTION booker_joins_meeting();
+
+--FIXES 21
+CREATE OR REPLACE FUNCTION block_outsiders_approval() RETURNS TRIGGER AS $$
+DECLARE
+    count NUMERIC;
+BEGIN
+    SELECT COUNT(*) into count;
+    FROM Employees, Meeting_Rooms
+    WHERE NEW.eid = Employees.eid AND NEW.date = Meeting_Rooms.date AND NEW.time = Meeting_Rooms.time
+    AND NEW.room = Meeting_Rooms.room AND NEW.floor = Meeting_Rooms.floor
+    AND Employees.did = Meeting_Rooms.did;
+
+    IF count > 0 THEN
+        RETURN NULL;
+    ELSE
+        RETURN NEW;
+    END IF;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER approval_only_from_same_department
+BEFORE INSERT OR UPDATE ON Approves
+FOR EACH ROW EXECUTE FUNCTION block_outsiders_approval();
+
+--FIXES 23
+CREATE OR REPLACE FUNCTION block_changes_after_approval() RETURNS TRIGGER AS $$
+DECLARE
+    count NUMERIC;
+BEGIN
+    SELECT COUNT(*) into count;
+    FROM Approves
+    WHERE NEW.date = Approves.date AND NEW.time = Approves.time AND NEW.room = Approves.room
+    AND NEW.floor = Approves.floor
+
+    IF count > 0 THEN
+        RETURN NULL;
+    ELSE
+        RETURN NEW;
+    END IF;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER no_updates_on_joins_after_approval
+BEFORE INSERT OR UPDATE ON Joins
+FOR EACH ROW EXECUTE FUNCTION block_changes_after_approval();
+
+--FIXES 34
+CREATE OR REPLACE FUNCTION block_resigned_employees() RETURNS TRIGGER AS $$
+DECLARE
+    count NUMERIC;
+BEGIN
+    SELECT COUNT(*) into count;
+    FROM Employees
+    WHERE NEW.eid = Employees.eid AND Employees.resigned_date IS NOT NULL;
+
+    IF count > 0 THEN
+        RETURN NULL;
+    ELSE
+        RETURN NEW;
+    END IF;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER a_resigned_employee_cannot_join
+BEFORE INSERT OR UPDATE ON Joins
+FOR EACH ROW EXECUTE FUNCTION block_resigned_employees();
+
+CREATE TRIGGER a_resigned_employee_cannot_approve
+BEFORE INSERT OR UPDATE ON Approves
+FOR EACH ROW EXECUTE FUNCTION block_resigned_employees();
+
+
