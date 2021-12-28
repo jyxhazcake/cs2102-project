9d9a647cb77681c064fcb4a563fe9db9ccb984c5
Jia Yixuan <jyxhazcake@yahoo.com>
2021-12-28 19:23:45 +0800

    Create client (frontend) package + Implement  AllDepartments page with basic functions

---
+++ client/src/components/AllDepartments/List.js
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from 'react'
+
+export default function List() {
+  const [departments, setDepartments] = useState([])
+
+  const getDepartments = async () => {
+    try {
+      const response = await fetch('http://localhost:3000/departments')
+      const jsonData = await response.json()
+
+      setDepartments(jsonData)
+    } catch (err) {
+      console.error(err.message)
+    }
+  }
+
+  useEffect(() => {
+    getDepartments()
+  }, [])
+
+  return (
+    <>
+      <table className="table table-dark">
+        <thead className="thead-light">
+          <tr>
+            <th scope="col">Department ID</th>
+            <th scope="col">Department Name</th>
+          </tr>
+        </thead>
+        <tbody>
+          {departments.map((dept) => (
+            <tr key={dept.did}>
+              <td>{dept.did}</td>
+              <td>{dept.dname}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
+  )
+}
---
+++ client/src/pages/AllDepartments/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Input from '../../components/AllDepartments/Input'
+import List from '../../components/AllDepartments/List'
+
+export default function AllDepartments() {
+  return (
+    <>
+      <h1>All Departments</h1>
+      <List />
+      <Input />
+    </>
+  )
+}
