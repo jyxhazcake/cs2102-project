f887458fc0d1f27b4404118d48ed13c224a89fec
goalfix <weihowe99@gmail.com>
2022-01-03 19:38:38 +0800

    changed to heroku BE for calls

--- client/src/components/AllDepartments/Input.js
+++ client/src/components/AllDepartments/Input.js
@@ -12,7 +12,7 @@ export default function Input() {
         did: did,
         dname: dname,
       }
-      const response = await fetch('http://localhost:3000/departments', {
+      const response = await fetch('https://cs2102-project-demo.herokuapp.com/departments', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(body),
@@ -29,7 +29,7 @@ export default function Input() {
   //Deleting a department with id
   const handleDelete = async (id) => {
     try {
-      const response = await fetch(`http://localhost:3000/departments/${id}`, {
+      const response = await fetch(`https://cs2102-project-demo.herokuapp.com/departments/${id}`, {
         method: 'DELETE',
       })
       console.log(response)
