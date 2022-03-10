5ca8382717024c6c61da85b3d1734a0d7d353e20
Yap Wei Xuan <e0533997@u.nus.edu>
2022-03-10 21:54:46 +0800

    Create server folder
    Move server related files (index.js) into a server folder

--- client/src/pages/LandingPage/index.js
+++ client/src/pages/LandingPage/index.js
@@ -2,27 +2,15 @@ import React from 'react'
 import { useNavigate } from 'react-router-dom'
 
 export default function LandingPage() {
-  const navigate = useNavigate()
-
-  return (
-    <div>
-      <h1> Landing Page </h1>
-      <div className="">
-        <button
-          type="button"
-          className="btn btn-dark"
-          onClick={() => navigate('/departments')}
-        >
-          Departments
-        </button>
-        <button
-          type="button"
-          className="btn btn-dark"
-          onClick={() => navigate('/employees')}
-        >
-          Employees
-        </button>
+    const navigate = useNavigate()
+  
+    return (
+      <div>
+        <h1> Landing Page </h1>
+        <div className="">
+          <input type = "text" name = "Username" />
+          <input type = "text" name = "Password" />
+        </div>
       </div>
-    </div>
-  )
-}
+    )
+  }
--- client/src/routes/Routes.js
+++ client/src/routes/Routes.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Route, Routes as Switch } from 'react-router-dom'
 
+import AdminPage from '../pages/AdminPage'
 import AllDepartments from '../pages/AllDepartments'
 import LandingPage from '../pages/LandingPage'
 import AllEmployees from '../pages/AllEmployees'
@@ -9,6 +10,7 @@ import EmployeeProfile from '../pages/EmployeeProfile'
 export const Routes = () => (
   <Switch>
     <Route path="/" element={<LandingPage />} />
+    <Route path="/admin" element={<AdminPage />} />
     <Route path="/departments" element={<AllDepartments />} />
     <Route path="/employees" element={<AllEmployees />} />
     <Route path="/profile/:id" element={<EmployeeProfile />} />
