e6c5bff3502d77587b26488ae71be77c91178b55
Yap Wei Xuan <e0533997@u.nus.edu>
2022-04-14 01:50:30 +0800

    update

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ const { nextTick } = require('process')
 
 app.use(cors())
 app.use(express.json())
-app.use(express.static(path.resolve(__dirname, 'client/build')))
+app.use(express.static(path.resolve(__dirname, '../client/build')))
 
 /*##################
   # Authentication #
